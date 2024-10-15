052add3c1385a48a7a0594973cf5529ff0cbf946
leedy-dev <ldy033000@gmail.com>
2024-10-15 09:18:21 +0900

    테마 설정 리팩토링

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -5,13 +5,23 @@ import { themeCreator } from './base';
 
 export const ThemeContext = createContext((themeName: string): void => {});
 
+const themeList = ['PureLightTheme', 'NebularFighterTheme'];
+
+const APP_THEME = 'appTheme';
+
 function ThemeProviderWrapper(props: { children: ReactElement }) {
   const { children } = props;
-  const curThemeName = localStorage.getItem('appTheme') || 'PureLightTheme';
-  const [themeName, _setThemeName] = useState(curThemeName);
+
+  const appTheme = localStorage.getItem(APP_THEME);
+
+  const curThemeName = themeList.includes(appTheme) && appTheme || themeList[0];
+
+  const [themeName, _setThemeName] = useState<string>(curThemeName);
+
   const theme = themeCreator(themeName);
+
   const setThemeName = useCallback((themeName: string) => {
-    localStorage.setItem('appTheme', themeName);
+    localStorage.setItem(APP_THEME, themeName);
     _setThemeName(themeName);
   }, []);
 
